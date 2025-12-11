39c5cb4b689a9f5f9a896c3246bb794dad409daa
Louis King <jinglemansweep@gmail.com>
2025-12-11 18:38:56 +0000

    Push

--- home-modules/desktop/browsers.nix
+++ home-modules/desktop/browsers.nix
@@ -10,6 +10,43 @@
         ublock-origin
         bitwarden
       ];
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "Toolbar";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "Cloud";
+                bookmarks = [
+                  { name = "GitHub"; url = "https://github.com"; }
+                  { name = "Infisical"; url = "https://app.infisical.com"; }
+                  { name = "Backblaze"; url = "https://secure.backblaze.com/b2_buckets.htm"; }
+                ];
+              }
+              { name = "Home Assistant"; url = "https://hass.ptre.uk"; }
+              { name = "NAS"; url = "https://nas.ptre.uk"; }
+              {
+                name = "Proxmox";
+                bookmarks = [
+                  { name = "PVM1"; url = "https://pvm1.ptre.uk"; }
+                  { name = "PVM2"; url = "https://pvm2.ptre.uk"; }
+                  { name = "PVM3"; url = "https://pvm3.ptre.uk"; }
+                ];
+              }
+              {
+                name = "IPNet";
+                bookmarks = [
+                  { name = "GitHub"; url = "https://github.com/ipnet-mesh"; }
+                  { name = "Website"; url = "https://ipnt.uk"; }
+                  { name = "Beta"; url = "https://beta.ipnt.uk"; }
+                ];
+              }
+            ];
+          }
+        ];
+      };
       settings = {
         # Privacy settings
         "privacy.trackingprotection.enabled" = true;
