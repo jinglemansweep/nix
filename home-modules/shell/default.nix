ccc7a36dc0044bffd0bd0092e964d739556e1caa
Louis King <jinglemansweep@gmail.com>
2025-12-11 18:08:33 +0000

    Push

--- home-modules/shell/default.nix
+++ home-modules/shell/default.nix
@@ -42,90 +42,33 @@
     keychain
   ];
 
-  # Starship prompt (Powerline style)
+  # Starship prompt (Minimal style)
   programs.starship = {
     enable = true;
     enableBashIntegration = true;
     settings = {
-      format = "$username$hostname$directory$git_branch$git_status$python$nodejs$golang$rust$terraform$docker_context$cmd_duration$line_break$character";
+      format = "$directory$git_branch$git_status$character";
+      add_newline = false;
 
       character = {
-        success_symbol = "[](bold green)";
-        error_symbol = "[](bold red)";
-      };
-
-      username = {
-        style_user = "bg:blue fg:black";
-        style_root = "bg:red fg:white";
-        format = "[ $user ]($style)[](bg:purple fg:blue)";
-        show_always = true;
-      };
-
-      hostname = {
-        ssh_only = false;
-        style = "bg:purple fg:black";
-        format = "[ $hostname ]($style)[](bg:cyan fg:purple)";
+        success_symbol = "[❯](green)";
+        error_symbol = "[❯](red)";
       };
 
       directory = {
-        style = "bg:cyan fg:black";
-        format = "[ $path ]($style)[](fg:cyan)";
+        style = "cyan";
         truncation_length = 3;
-        truncate_to_repo = true;
+        truncate_to_repo = false;
       };
 
       git_branch = {
-        style = "bg:green fg:black";
-        format = "[](fg:green)[ $symbol$branch ]($style)";
-        symbol = " ";
+        style = "purple";
+        format = "[$branch]($style) ";
       };
 
       git_status = {
-        style = "bg:green fg:black";
-        format = "[$all_status$ahead_behind]($style)[](fg:green)";
-      };
-
-      cmd_duration = {
-        min_time = 2000;
-        style = "bg:yellow fg:black";
-        format = "[](fg:yellow)[ $duration ]($style)[](fg:yellow)";
-      };
-
-      python = {
-        style = "bg:yellow fg:black";
-        format = "[](fg:yellow)[ $symbol$version ]($style)[](fg:yellow)";
-        symbol = " ";
-      };
-
-      nodejs = {
-        style = "bg:green fg:black";
-        format = "[](fg:green)[ $symbol$version ]($style)[](fg:green)";
-        symbol = " ";
-      };
-
-      golang = {
-        style = "bg:cyan fg:black";
-        format = "[](fg:cyan)[ $symbol$version ]($style)[](fg:cyan)";
-        symbol = " ";
-      };
-
-      rust = {
-        style = "bg:red fg:white";
-        format = "[](fg:red)[ $symbol$version ]($style)[](fg:red)";
-        symbol = " ";
-      };
-
-      terraform = {
-        style = "bg:purple fg:white";
-        format = "[](fg:purple)[ $symbol$version ]($style)[](fg:purple)";
-        symbol = "󱁢 ";
-      };
-
-      docker_context = {
-        style = "bg:blue fg:white";
-        format = "[](fg:blue)[ $symbol$context ]($style)[](fg:blue)";
-        symbol = " ";
-        only_with_files = true;
+        style = "red";
+        format = "[$all_status$ahead_behind]($style)";
       };
     };
   };
