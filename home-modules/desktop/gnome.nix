9cb7895606dba18de299c702c960ae9f945f606f
Louis King <jinglemansweep@gmail.com>
2025-12-12 10:41:59 +0000

    Push

--- home-modules/desktop/gnome.nix
+++ home-modules/desktop/gnome.nix
@@ -1,6 +1,18 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Disable GNOME Keyring SSH agent (using keychain instead)
+  services.gnome-keyring.enable = false;
+
+  # Disable GCR SSH agent socket/service
+  systemd.user.services.gcr-ssh-agent = {
+    Unit.Description = "Disabled GCR SSH Agent";
+    Service.ExecStart = "${pkgs.coreutils}/bin/true";
+  };
+  systemd.user.sockets.gcr-ssh-agent = {
+    Install.WantedBy = lib.mkForce [];
+  };
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -25,7 +25,7 @@
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Networking
