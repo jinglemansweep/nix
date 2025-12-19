80e7c5e471bd2047318cba378248eb77dc7f3186
Louis King <jinglemansweep@gmail.com>
2025-12-19 17:19:13 +0000

    Push

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -4,7 +4,18 @@
   imports = [
     ./browsers.nix
     ./vscode.nix
-    ./apps.nix
     ./gnome.nix
+  ];
+
+  home.packages = [
+    # Office suite
+    pkgs.libreoffice
+
+    # Graphics editors
+    pkgs.mtpaint
+    pkgs.gimp
+
+    # Terminal emulators (additional to gnome-terminal)
+    pkgs.rxvt-unicode
   ];
 }
