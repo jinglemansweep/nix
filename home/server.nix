ffaf6a0c2096d49a1662a16ae9e38fbdcaec7be7
Louis King <jinglemansweep@gmail.com>
2026-04-08 00:44:07 +0100

    Updates

--- home/server.nix
+++ home/server.nix
@@ -8,4 +8,16 @@
     ../modules/home/shell/docker.nix
     ../modules/home/shell/development.nix
   ];
+
+  home.packages = [ pkgs.vscode ];
+
+  systemd.user.services.code-tunnel = {
+    Unit.Description = "VS Code Tunnel";
+    Service = {
+      ExecStart = "${pkgs.vscode}/bin/code tunnel --accept-server-license-terms";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
 }
