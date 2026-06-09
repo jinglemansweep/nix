9f8321fedc43fe59f93d884f7c95645960803487
Louis King <jinglemansweep@gmail.com>
2026-06-09 12:00:05 +0100

    Updates

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -71,6 +71,12 @@
   services = {
     openssh = {
       enable = true;
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
       settings = {
         PasswordAuthentication = false;
         PermitRootLogin = "no";
